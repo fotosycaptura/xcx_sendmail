055a3b45b2c967a9eefafd9b2fc2b9b2c92157ea
fotosycaptura <charsxavier@gmail.com>
2021-08-31 16:09:22 -0400

    Adding fillna('')

--- classes/datos_csv.py
+++ classes/datos_csv.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 class datos_csv():
     def get_rectores():
-        rectores = pd.read_csv('./datos/datos.csv', header = 0,delimiter =';', encoding='utf-8-sig', index_col=0)
+        rectores = pd.read_csv('./datos/datos.csv', header = 0,delimiter =';', encoding='utf-8-sig', index_col=0).fillna('')
         return rectores 
